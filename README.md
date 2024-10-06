34f69d8e15e7f07c72fb3f91ded44f35115d15d0
Notcookiezi <joshua.john.payson@gmail.com>
2024-10-06 18:06:06 -0400

    love you

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IdleGame
 NathanJoshJacobIdleGame
+hi
--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
 
 --runs once at the start of the game (for setup)
 function love.load()
-
-
+    sprite=love.graphics.newImage("Women_idle_person_1.png")
+    love.window.setTitle("ungabogga")
 end
 
 --runs every frame (for logic)
@@ -13,5 +13,5 @@ end
 
 --runs every frame (draws to screen) 
 function love.draw()
-
+    love.graphics.draw(sprite)
 end
