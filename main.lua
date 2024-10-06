8112d46b1545f91ddd5a7d940ae3b5f1e35034a8
Nathan Gerryts <nathgerr@gmail.com>
2024-10-06 17:06:25 -0400

    Init

---
+++ main.lua
@@ -0,0 +1,17 @@
+
+
+--runs once at the start of the game (for setup)
+function love.load()
+
+
+end
+
+--runs every frame (for logic)
+function love.update(dt)
+
+end
+
+--runs every frame (draws to screen) 
+function love.draw()
+
+end
